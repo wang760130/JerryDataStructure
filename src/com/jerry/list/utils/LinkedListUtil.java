b74414e10648b7c906355e729ee9ef832755d3c7
JerryWang <jerry002@126.com>
2014-11-08 22:54:01 +0800

    单链表工具类

---
+++ src/com/jerry/list/utils/LinkedListUtil.java
@@ -0,0 +1,42 @@
+package com.jerry.list.utils;
+
+import com.jerry.list.LinkedList;
+
+/**
+ * 单链表工具类
+ * @author Jerry Wang
+ * @param <T>
+ *
+ */
+
+public class LinkedListUtil {
+	/**
+	 * 两个单链表归并，生成新的链表
+	 * @author Jerry Wang
+	 *
+	 */
+	public static LinkedList<Integer> mergeList(LinkedList<Integer> listA, LinkedList<Integer> listB) {
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		int i = 0;
+		int j = 0;
+		while(i < listA.size() && j < listB.size()) {
+			Integer ta = listA.get(i);
+			Integer tb = listB.get(j);
+			if(ta <= tb) {
+				list.add(ta);
+				i++;
+			} else {
+				list.add(tb);
+				j++;
+			}
+		}
+		while(i < listA.size()) 
+			list.add(listA.get(i++));
+		
+		while(j < listB.size()) 
+			list.add(listB.get(j++));
+		
+		return list;
+	}
+	
+}
