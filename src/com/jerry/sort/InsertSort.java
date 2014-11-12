ec3d101fab91150be89521a64d27e13990d3110c
JerryWang <jerry002@126.com>
2014-11-12 22:23:48 +0800

    插入排序

--- src/com/jerry/sort/InsertSort.java
+++ src/com/jerry/sort/InsertSort.java
@@ -10,8 +10,10 @@ public class InsertSort extends Sort{
 	@Override
 	public void sort(int[] data) {
 		for (int i = 1; i < data.length; i++) {
-			for (int j = i; (j > 0) && (data[j] < data[j-1]); j--)
+			for (int j = i; (j > 0) && (data[j] < data[j-1]); j--) {
 				super.swap(data, j, j - 1);
+				super.display(data);
+			}
 		}
 	}
 
