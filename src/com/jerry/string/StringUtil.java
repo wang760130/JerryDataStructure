359230b6867428c912e01050b169628ce9fde0c9
JerryWang <jerry002@126.com>
2014-11-15 23:04:52 +0800

    字符串操作工具类

--- src/com/jerry/string/StringUtil.java
+++ src/com/jerry/string/StringUtil.java
@@ -6,6 +6,50 @@ package com.jerry.string;
  *
  */
 public class StringUtil {
+	public static boolean equal() {
+		return false;
+	}
+	
+	public static boolean isEmpty() {
+		return false;
+	}
+	
+	public static int length() {
+		return 0;
+	}
+	
+	public static String concat(String str1, String str2) {
+		return null;
+	}
+	
+	public static String subString(String str, int startIndex) {
+		return null;
+	}
+	
+	
+	public static String subString(String str, int startIndex, int endIndex) {
+		return null;
+	}
+	
+	public static char index(String str, int index) {
+		return 'a';
+	}
+	
+	public static int index(String str, char ch) {
+		return 0;
+	}
+	
+	public static String repacle(String str, int index, char ch) {
+		return null;
+	}
+	
+	public static String insert(String str, int index, String str2) {
+		return null;
+	}
+	
+	public static String delete(String str, int index, int length) {
+		return null;
+	}
 	
 	
 }
