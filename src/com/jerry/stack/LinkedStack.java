5a8a81254e23257a679e41a73eac101b3945b349
JerryWang <jerry002@126.com>
2014-11-17 23:46:01 +0800

    使用链表实现栈

--- src/com/jerry/stack/LinkedStack.java
+++ src/com/jerry/stack/LinkedStack.java
@@ -6,47 +6,79 @@ package com.jerry.stack;
  *
  */
 public class LinkedStack<T> implements Stack<T> {
-
+	
+	public static class Node<T> {
+		private T data;
+		private Node<T> next;
+		
+		public Node() {
+			this.data = null;
+			this.next = null;
+		}
+		
+		public Node(T data) {
+			this.data = data;
+			this.next = null;
+		}
+		
+		public Node(T data, Node<T> next) {
+			this.data = data;
+			this.next = next;
+		}
+	}
+	
+	private Node<T> top = null;
+	private int size = 0;
+	
+	public LinkedStack() {
+		this.top = null;
+		this.size = 0;
+	}
+	
+	public LinkedStack(T data) {
+		this();
+		Node<T> node = new Node<T>(data);
+		this.top = node;
+		this.size++;
+	}
+	
 	@Override
-	public T push(T item) {
-		// TODO Auto-generated method stub
-		return null;
+	public void push(T item) {
+		Node<T> node = new Node<T>(item,top);
+		this.top = node;
+		this.size++;
 	}
 
 	@Override
 	public T pop() {
-		// TODO Auto-generated method stub
-		return null;
+		Node<T> oldTop = this.top;
+		if(null == top) {
+			return null;
+		}
+		this.top = this.top.next;
+		this.size--;
+		return oldTop.data;
 	}
 
 	@Override
 	public T top() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.top.data;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public int search(Object o) {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.top == null;
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.size;
 	}
 
 	@Override
-	public int clear() {
-		// TODO Auto-generated method stub
-		return 0;
+	public void clear() {
+		this.top = null;
+		this.size = 0;
 	}
 
 }
