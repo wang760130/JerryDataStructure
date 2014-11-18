e936e08aa7ccacbf79b3e5d4b9f28b1984a7c447
JerryWang <jerry002@126.com>
2014-11-18 22:18:57 +0800

    链表实现二叉树

---
+++ src/com/jerry/binarytree/LinkedBinaryTree.java
@@ -0,0 +1,57 @@
+package com.jerry.binarytree;
+
+/**
+ * 链表实现二叉树
+ * @author Jerry Wang
+ *
+ */
+public class LinkedBinaryTree<T> implements BinaryTree<T>{
+
+	@Override
+	public void add(int index, T data, boolean left) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean empty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public T getRoot() {
+		 return null;
+	}
+	
+	@Override
+	public T getParent(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T getLeft(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T getRight(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int deep() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int position(T data) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
