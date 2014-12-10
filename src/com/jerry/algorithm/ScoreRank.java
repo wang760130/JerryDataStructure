33546f3eee34880ff21cf51cdd394444fb05a8bb
JerryWang <jerry002@126.com>
2014-12-11 00:27:17 +0800

    得分排行

--- src/com/jerry/algorithm/ScoreRank.java
+++ src/com/jerry/algorithm/ScoreRank.java
@@ -1,37 +1,29 @@
 package com.jerry.algorithm;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 /**
  * 得分排行
  * @author Jerry Wang
  * 
  */
 public class ScoreRank {
-	public static void main(String[] args) throws NumberFormatException,
-			IOException {
+	public static void getScoreRank(int[] score) {
 		final int MAX = 100;
-		final int MIN = 0;
-		int[] score = new int[MAX + 1];
-		int[] juni = new int[MAX + 2];
-		BufferedReader reader = new BufferedReader(new InputStreamReader(
-				System.in));
-		int count = 0;
-		do {
-			System.out.print("输入分数，-1结束：");
-			score[count++] = Integer.parseInt(reader.readLine());
-		} while ((score[count - 1] != -1));
-		count--;
-		for (int i = 0; i < count; i++)
-			juni[score[i]]++;
-		juni[MAX + 1] = 1;
-		for (int i = MAX; i >= MIN; i--)
-			juni[i] = juni[i] + juni[i + 1];
+		int[] rank = new int[MAX+2];
+		int length = score.length;
+		for (int i = 0; i < length; i++)
+			rank[score[i]]++;
+
+		rank[MAX + 1] = 1;
+		for (int i = MAX; i >= 0; i--)
+			rank[i] = rank[i] + rank[i + 1];
 		System.out.println("得分\t排行");
-		for (int i = 0; i < count; i++) {
-			System.out.println(score[i] + "\t" + juni[score[i] + 1]);
+		for (int i = 0; i < length; i++) {
+			System.out.println(score[i] + "\t" + rank[score[i] + 1]);
 		}
+		
+	}
+	public static void main(String[] args)  {
+		int[] score = new int[] {0,40,30,80,88,70,30,45,2,34,100,99};
+		ScoreRank.getScoreRank(score);
 	}
 }
