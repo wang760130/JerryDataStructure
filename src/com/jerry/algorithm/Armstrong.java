3e4139ac57238cd0482780da3a87ef8f688320f0
JerryWang <jerry002@126.com>
2014-12-13 23:47:04 +0800

    阿姆斯壮数

--- src/com/jerry/algorithm/Armstrong.java
+++ src/com/jerry/algorithm/Armstrong.java
@@ -8,7 +8,7 @@ package com.jerry.algorithm;
  */
 public class Armstrong {
 	public static void main(String[] args) {
-		System.out.println("寻找Armstrong数：");
+		System.out.println("寻找Armstrong数：  ");
 		for (int i = 100; i <= 999; i++) {
 			int a = i / 100;
 			int b = (i % 100) / 10;
